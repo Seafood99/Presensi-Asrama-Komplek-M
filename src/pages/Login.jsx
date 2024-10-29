726cb76511cc2349e3f016658b7f302c0d34df6c
Dawam Amali <130631491+Seafood99@users.noreply.github.com>
2024-10-29 12:26:23 +0700

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
 
-        const response = await fetch('http://localhost:4100/login', {
+        const response = await fetch('https://presensi-asrama-komplek-m-production.up.railway.app/api/santri', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
